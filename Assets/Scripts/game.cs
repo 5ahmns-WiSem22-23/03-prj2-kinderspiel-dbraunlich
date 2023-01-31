8a5d30928210ce877e27a91654cf6edbe21adac6
dbraunlich <72389971+dbraunlich@users.noreply.github.com>
2023-01-31 17:51:23 +0100

    added graphics

--- Assets/Scripts/game.cs
+++ Assets/Scripts/game.cs
@@ -5,8 +5,7 @@ using TMPro;
 
 public class game : MonoBehaviour
 {
-
-
+    bool anyObjectReached = false;
     public static string winner;
     public GameObject object1, object2, object3, object4, boat;
     private int diceRoll;
@@ -26,13 +25,30 @@ public class game : MonoBehaviour
             RollDice();
         }
         CheckReached();
+
+        if (object1Reached == true || object2Reached == true || object3Reached == true || object4Reached == true)
+        {
+        anyObjectReached = true;
+        }
+
+        if (boat.transform.position.x >= 3)
+        {
+        Debug.Log("Das Boot gewinnt");
+        }
+        else if (object1.transform.position.x >= 3 && object2.transform.position.x >= 3 && object3.transform.position.x >= 3 && object4.transform.position.x >= 3)
+        {
+        Debug.Log("Die Fische gewinnen");
+        }
+
+
+
     }
 
     void RollDice() {
         diceRoll = Random.Range(1, 7); // Würfelwert zwischen 1 und 6
         Debug.Log("Dice Roll: " + diceRoll);
 
-        if (diceRoll >= 1 && diceRoll <= 4 && object1Reached ==false) {
+        if (diceRoll >= 1 && diceRoll <= 4) {
             if (diceRoll == 1) {
                 object1.transform.position += new Vector3(1, 0, 0);
             } else if (diceRoll == 2) {
@@ -47,7 +63,7 @@ public class game : MonoBehaviour
             boat.transform.position += new Vector3(1, 0, 0);
         }
     
-        if (diceRoll >= 1 && diceRoll <= 4 && object1Reached) {
+        if (diceRoll >= 1 && diceRoll <= 4 && anyObjectReached) {
             if (diceRoll == 1 && object1Reached) {
                 boat.transform.position += new Vector3(1, 0, 0);
                 Debug.Log("beide bewegt");
@@ -71,21 +87,25 @@ public class game : MonoBehaviour
         {
             object1Reached = true;
             object1.SetActive(false);
+
         }
         if (object2.transform.position.x == boat.transform.position.x)
         {
             object2Reached = true;
             object2.SetActive(false);
+
         }
         if (object3.transform.position.x == boat.transform.position.x)
         {
             object3Reached = true;
             object3.SetActive(false);
+ 
         }
         if (object4.transform.position.x == boat.transform.position.x)
         {
             object4Reached = true;
             object4.SetActive(false);
+
         }
     }
 
